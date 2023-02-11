b06e20709b68db4e8210ae8004a7e5aa00e69b6a
Sathish <sathishbabur@hotmail.com>
2023-02-11 19:13:01 +0530

    Generated random numbers

--- src/app/basic-component/basic-component.component.ts
+++ src/app/basic-component/basic-component.component.ts
@@ -7,9 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BasicComponentComponent implements OnInit {
 
+  private sums: number[][] = [];
+  statusMessage: string = "";
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  generate() {
+
+    for (let sumIndex = 0; sumIndex < 5; sumIndex++) {
+      let aSum: number[] = []
+      for (let rowIndex = 0; rowIndex < 5; rowIndex++) {
+        let randomValue = this.uniqueNumberGenerator(9);
+        randomValue *= this.negativeValueProbality();
+        aSum.push(randomValue);
+      }
+      this.sums.push(aSum);
+    }
+    this.statusMessage = this.sums.toString();
+  }
+  negativeValueProbality(): number {
+    let anyValue = this.uniqueNumberGenerator(9);
+    return anyValue % 5 == 0 ? -1 : 1;
+  }
+
+  uniqueNumberGenerator(maxValue: number) {
+    return Math.floor((Math.random() * maxValue) + 1);
+  }
+
 
 }
